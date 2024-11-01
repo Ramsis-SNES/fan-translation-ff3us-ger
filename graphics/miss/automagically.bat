09d566abc2e5592c5f1b78feedde3192ee648c3e
Ramsis-SNES <ramsis@manuloewe.de>
2024-11-01 12:25:20 +0100

    Reorganize folder structure and put project on GitHub

---
+++ graphics/miss/automagically.bat
@@ -0,0 +1,6 @@
+cls
+@echo compress d ff3.smc 12e200 > blabla.txt
+@echo check-out decomp.bin and modify it in your favorite tile editor, come back when you are finished!! (P.S. make sure you "close" the file if using TileEd ;)
+pause
+@echo compress c ff3.smc 12e200 decomp.bin > blabla2.txt
+@echo it's time to take a closer look at your modif in your emu!! Bye!!
